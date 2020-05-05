69c699c6c282de9637c49a52e2e527a8b89cd7d9
arozycka <agnieszka.rozycka2018@gmail.com>
2020-05-05 21:01:55 +0200

    marge.Contact

--- src/elektryk/Contact.js
+++ src/elektryk/Contact.js
@@ -9,13 +9,14 @@ class Contact extends React.Component {
             <div className="mainDivContact">
                 <div className="headerContact"><p>KONTAKT</p>
                 </div>
-                <div className="setKontakt">
+                <div className="setMaps">
+                    <div className="setKontakt">
                     <spam className="logoKontakt">
                         <p className="dwieLitery">OM</p>
                         <p className="trzyLitery">TOM</p>
-                        <p className="name"> - Tomasz Żurek</p>
                     </spam>
                     <div>
+                        <p className="name">Tomasz Żurek</p>
                         <p className="tel">tel: 668 847 119</p>
                         <p className="mail">mail: biuro@omtom.pl</p>
                         <p className="adresKontakt">ul. Morska 1 </p>
@@ -24,11 +25,14 @@ class Contact extends React.Component {
                         <p className="adresKontakt" id="nip">NIP 5130068329</p>
                     </div>
                 </div>
-                < div className="contact-map">
-                    {/*<iframe*/}
-                    {/*    src="https://www.google.com/maps/place/OMTOM+Tomasz+%C5%BBurek/@50.1687629,19.8351582,17z/data=!3m1!4b1!4m5!3m4!1s0x471657d7284dc803:0xabb2669bd5d65d52!8m2!3d50.1687595!4d19.8373469"*/}
-                    {/*    width="400" height="400"></iframe>*/}
+                    <div className="contact-map">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2555.5923873345205!2d19.835158215374317!3d50.16875947943751!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471657d7284dc803%3A0xabb2669bd5d65d52!2sOMTOM%20Tomasz%20%C5%BBurek!5e0!3m2!1spl!2sch!4v1588704354713!5m2!1spl!2sch"
+                            width="600" height="450" frameBorder="0" allowFullScreen=""
+                            aria-hidden="false" tabIndex="0"></iframe>
+                    </div>
                 </div>
+
             </div>
 
 
